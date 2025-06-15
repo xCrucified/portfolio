dbb40bb28bdb05fc72397a641822c4aeff17cd15
xCrucified <eexet17@gmail.com>
2025-06-15 03:55:25 +0200

    add header animation effects and refactor link structure for improved interactivity

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import React, { useState } from "react";
@@ -8,6 +8,11 @@ import { Container } from "./container";
 interface Props {
   className?: string;
 }
+const links = [
+  { href: "/", label: "about" },
+  { href: "/", label: "contact" },
+  { href: "/", label: "resume" },
+];
 
 export const Header: React.FC<Props> = ({ className }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -15,17 +20,26 @@ export const Header: React.FC<Props> = ({ className }) => {
 
   return (
     <div className="w-[100%] h-[180px] outline">
-      <Container className={cn("flex w-[100%] h-[100%] justify-between items-center outline-3", className)}>
-        <Link href={"/"} className="font-bold text-5xl text-white">
+      <Container
+        className={cn(
+          "flex w-[100%] h-[100%] justify-between items-center outline-3",
+          className
+        )}
+      >
+        <Link
+          href={"/"}
+          className="font-bold text-5xl text-white header-animation"
+        >
           Portfolio
         </Link>
 
         <div className="flex items-center gap-4 text-white font-bold text-lg">
-          <Link href={"/"}>about</Link>
-          <Link href={"/"}>contact</Link>
-          <Link href={"/"}>resume</Link>
+          {links.map((link, index) => (
+            <Link key={index} href={link.href} className="header-animation">
+              {link.label}
+            </Link>
+          ))}
         </div>
-
       </Container>
     </div>
   );
