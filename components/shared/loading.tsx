c1cb3930439ff220b7192799d3162532f58ccd72
xCrucified <eexet17@gmail.com>
2026-05-18 00:29:00 +0200

    refactor: enhance Loading component with improved styling and layout

--- components/shared/loading.tsx
+++ components/shared/loading.tsx
@@ -6,9 +6,15 @@ interface Props {
 
 export const Loading: React.FC<Props> = ({ className }) => {
   return (
-    <div className={`flex items-center justify-center h-screen ${className}`}>
-      <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
-      <span className="ml-4 text-lg text-gray-700">Loading...</span>
+    <div className={`flex flex-col items-center justify-center h-screen bg-[#130919] select-none ${className}`}>
+      <div className="relative flex items-center justify-center">
+        <div className="absolute animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#1FAC71] opacity-40 blur-xs"></div>
+        <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-[#7d1fac]"></div>
+      </div>
+      
+      <span className="mt-6 text-lg font-light text-[#b3a1bd] tracking-wider animate-pulse">
+        Loading...
+      </span>
     </div>
   );
 };
